fa5c2f79c27d0c845ef367d11c8071c8aa4dd7cd
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2022-01-06 08:55:32 -0500

    Fix issues from 'set -e' combined with pipefail
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- scripts/hugo-audit-minified.sh
+++ scripts/hugo-audit-minified.sh
@@ -8,7 +8,7 @@ if HUGO_MINIFY_TDEWOLFF_HTML_KEEPCOMMENTS=true HUGO_ENABLEMISSINGTRANSLATIONPLAC
 then
     # If hugo build succeeds but possible audit issues are present, check further
     # Check for problem indicators (see https://discourse.gohugo.io/t/audit-your-published-site-for-problems/35184)
-    grep -iIrnoE "<\!-- raw HTML omitted -->|ZgotmplZ|hahahugo|\[i18n\]|\(<nil>\)|(&lt;nil&gt;)" public/ >hugo-audit.log; RET=$?
+    grep -iIrnoE '<\!-- raw HTML omitted -->|ZgotmplZ|hahahugo|\[i18n\]|\(<nil>\)|\(&lt;nil&gt;\)' public/ >hugo-audit.log; RET=$?
     if [ "$RET" != "1" ] && [ "$RET" != "0" ]
     then
         # Make sure we fail if there is is error executing the check command
@@ -20,7 +20,7 @@ then
     # to check for Hugo audit issues).
     if [ "$RET" = "0" ]
     then
-        grep -iIvE "grep(.+(-- raw HTML omitted --|ZgotmplZ|hahahugo|\\\[i18n\\\]|\\\(<nil>\\\)|(&lt;nil&gt;))+)" hugo-audit.log; RET2=$?
+        grep -iIvE 'grep(.+(-- raw HTML omitted --|ZgotmplZ|hahahugo|\[i18n\]|\(<nil>\)|\(&lt;nil&gt;\))+)' hugo-audit.log; RET2=$?
         if [ "$RET2" != "1" ]
         then
             # Make sure we fail if there is an error executing the false positive elimination command (exit code 2)
